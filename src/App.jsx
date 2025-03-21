e33ecfa3d58e7588e810728e16297660ab337a99
LeonelC98 <Leonel18costamagna@gmail.com>
2025-03-21 19:20:22 -0300

    agrege algunas cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
+import { useEffect } from "react"
 
 const Title =({something}) => <h1>{something}</h1>
-
 
 const Buttons =({handleGood,handleNeutral,handleBad}) =>{
   return(
@@ -14,12 +14,15 @@ const Buttons =({handleGood,handleNeutral,handleBad}) =>{
   )
 }
 
-const Statistics =({good,neutral,bad})=>{
+const Statistics =({good,neutral,bad,total,average,positive})=>{
   return(
     <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
 
     </>
   )
@@ -29,26 +32,47 @@ const App=()=> {
   const [good,setGood] = useState(0)
   const [neutral,setNeutral] = useState(0)
   const [bad,setBad] = useState(0)
-  
+  const [total,setTotal] = useState(0)
+  const [average,setAverage] = useState(0)
+  const [positive,setPositive] = useState(0)
+
+
   const handleGood =()=>{
     const goodPluss = good + 1
+    const totalPluss = total + 1
+    const averagePluss = (goodPluss - bad)/totalPluss
+    const positivePluss = goodPluss /totalPluss
     setGood(goodPluss)
+    setTotal(totalPluss)
+    setAverage(averagePluss)
+    setPositive(positivePluss)
   }
   const handleNeutral =()=>{
     const neutralPluss = neutral + 1
+    const totalPluss = total + 1
+    const averagePluss = (good - bad)/totalPluss
+    const positivePluss = good /totalPluss
     setNeutral(neutralPluss)
+    setTotal(totalPluss)
+    setAverage(averagePluss)
+    setPositive(positivePluss)
   }
   const handleBad =()=>{
     const badPluss = bad + 1
+    const totalPluss = total + 1
+    const averagePluss = (good - badPluss)/totalPluss
+    const positivePluss = good /totalPluss
     setBad(badPluss)
+    setTotal(totalPluss)
+    setAverage(averagePluss)
+    setPositive(positivePluss)
   }
   return (
     <>
     <Title something={"give feedback"}/>
     <Buttons handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad}/>
     <Title something={"statistics"}/>
-    <Statistics good={good} neutral={neutral} bad={bad}/>
-
+    <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
     </>
   )
 }
